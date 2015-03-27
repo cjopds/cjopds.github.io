b8c40382172bf67b1ae46a5b55d37fd2e5b5d763
CJOPDS <ruizh.cj+cjopds@gmail.com>
2015-03-27 14:03:51 +0100

    First catalog

---
+++ bibcal/_catalog/_search/database/identifier.js
@@ -0,0 +1,7 @@
+function getIdentifier () {
+ // id, label, date
+ // 1 elements
+ return [
+  ['ac9e3cbbc3c9e2ce:-5f8d7ec7:14c5b53aaee:-8000','Biblioteca de Calibre de CJ','27/03/15 14:02']
+ ];
+}
