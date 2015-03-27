2db19e19bb74bef7977b32b4c36a00c35ca4c302
CJOPDS <ruizh.cj+cjopds@gmail.com>
2015-03-27 14:17:46 +0100

    not ciphered

--- bibcal/_catalog/_search/database/identifier.js
+++ bibcal/_catalog/_search/database/identifier.js
@@ -2,6 +2,6 @@ function getIdentifier () {
  // id, label, date
  // 1 elements
  return [
-  ['ac9e3cbbc3c9e2ce:-5f8d7ec7:14c5b53aaee:-8000','Biblioteca de Calibre de CJ','27/03/15 14:02']
+  ['ac9e3cbbc3c9e2ce:-5f8d7ec7:14c5b53aaee:-7fff','Biblioteca de Calibre de CJ','27/03/15 14:17']
  ];
 }
